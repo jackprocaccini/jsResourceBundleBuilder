5a0d93a5e11d436b81c3a3457ebadcbe151a53c5
jackprocaccini <jproc1222@gmail.com>
2020-03-18 16:05:55 -0400

    XML Sax Parsing

--- src/main/java/com/github/jackprocaccini/Main.java
+++ src/main/java/com/github/jackprocaccini/Main.java
@@ -1,8 +1,16 @@
 package com.github.jackprocaccini;
 
+import com.github.jackprocaccini.xml.xmlPropertiesBuilder;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args){
-
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+        xmlPropertiesBuilder builder = new xmlPropertiesBuilder();
+        builder.readXMLFile(new File("src/main/resources/EmployeeHome.resx"));
     }
 }
--- src/main/java/com/github/jackprocaccini/xml/xmlPropertiesBuilder.java
+++ src/main/java/com/github/jackprocaccini/xml/xmlPropertiesBuilder.java
@@ -1,4 +1,63 @@
 package com.github.jackprocaccini.xml;
 
-public class xmlPropertiesBuilder {
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.File;
+import java.io.IOException;
+
+public class xmlPropertiesBuilder extends DefaultHandler {
+    private boolean dataFound;
+    private boolean valueFound;
+    private boolean dataCommentFound;
+
+    public void readXMLFile(File xmlFile) throws SAXException, IOException, ParserConfigurationException {
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        SAXParser parser = factory.newSAXParser();
+        parser.parse(xmlFile, this);
+        dataFound = false;
+        valueFound = false;
+    }
+
+    @Override
+    public void startDocument() throws SAXException {
+        System.out.println("Start Document");
+    }
+
+    @Override
+    public void endDocument() throws SAXException {
+        System.out.println("End Document");
+    }
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        if(qName.equals("data")){
+            System.out.println("Start Element: " + qName);
+            System.out.println("Key: " + attributes.getValue("name"));
+            dataFound = true;
+        } else if(qName.equals("value")){
+            valueFound = true;
+        }
+    }
+
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        if(qName.equals("data")){
+            System.out.println("End Element: " + qName + "\n");
+            dataFound = false;
+        }
+
+    }
+
+    @Override
+    public void characters(char[] ch, int start, int length) throws SAXException {
+        if(valueFound && dataFound){
+            System.out.println("Value: " + new String(ch, start, length));
+            valueFound = false;
+        }
+    }
 }
